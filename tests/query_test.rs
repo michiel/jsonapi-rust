8912561fd6fdcd5f1ab768718d88b5a64faf25ff
Michiel Kalkman <michiel@nosuchtype.com>
2017-02-21 16:01:42 +0100

    Add basic tests for query include and paging params

--- tests/query_test.rs
+++ tests/query_test.rs
@@ -7,6 +7,21 @@ use jsonapi::query::*;
 fn can_parse() {
     let query = Query::from_params("include=author&fields[articles]=title,\
                                     body&fields[people]=name&page[number]=3&page[size]=1");
-    println!("Received : {:?}", query);
-    assert_eq!(1, 1);
+
+    match query.include {
+        None => assert!(false),
+        Some(include) => {
+            assert_eq!(include.len(), 1);
+            assert_eq!(include[0], "author");
+        }
+    }
+
+    match query.page {
+        None => assert!(false),
+        Some(page) => {
+            assert_eq!(page.size, 1);
+            assert_eq!(page.number, 3);
+        }
+    }
+
 }
