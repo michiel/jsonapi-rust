95fe2953e7c7c63e6635f514ded5686693c67b58
Michiel Kalkman <michiel@nosuchtype.com>
2017-02-22 16:59:48 +0100

    refactor: Move file reader to helper

--- tests/api_test.rs
+++ tests/api_test.rs
@@ -3,28 +3,8 @@ extern crate serde_json;
 
 use jsonapi::api::*;
 
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
-pub fn read_json_file(filename: &str) -> String {
-    let path = Path::new(filename);
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
-        Ok(_) => {}
-    };
-
-    s
-}
+mod helper;
+use helper::read_json_file;
 
 #[test]
 fn it_works() {
