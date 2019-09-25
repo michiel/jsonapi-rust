875f6fd31273bd410210a9063b1d45679bfc3ec3
Alex G Rice <rice.guido@gmail.com>
2019-09-25 15:03:41 -0600

    refactor: remove test-related structs from src/model
    These were leftover test-related structs (Dog and Flea) in model.rs.

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,5 @@
 pub use std::collections::HashMap;
 pub use crate::api::*;
-use crate::array::JsonApiArray;
 use crate::errors::*;
 use serde::{Deserialize, Serialize};
 use serde_json::{from_value, to_value, Value, Map};
@@ -308,18 +307,3 @@ macro_rules! jsonapi_model {
         }
     );
 }
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Dog {
-    id: String,
-    name: String,
-    age: i32,
-    main_flea: Flea,
-    fleas: Vec<Flea>,
-}
-jsonapi_model!(Dog; "dog"; has one main_flea; has many fleas);
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Flea {
-    id: String,
-    name: String,
-}
-jsonapi_model!(Flea; "flea");
